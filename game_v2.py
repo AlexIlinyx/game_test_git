0dce69d14b868b45d80064ad53087bfd325c8aec
AlexIlinyx <ilinyx2@gmail.com>
2023-10-06 23:39:27 +1000

    222

--- game_v2.py
+++ game_v2.py
@@ -23,6 +23,23 @@ def random_predict(number: int = 1) -> int:
             break  # выход из цикла если угадали
     return count
 
+def binar_predict(number: int = 1) -> int:
+    """_summary_
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict_number:
+            break  # выход из цикла если угадали
+    return count
 
 def score_game(random_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
@@ -48,3 +65,4 @@ def score_game(random_predict) -> int:
 if __name__ == "__main__":
     # RUN
     score_game(random_predict)
+    score_game(binar_predict)
