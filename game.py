766d0edf83082f9828dd30c438c46dbd22a63c6c
AlexIlinyx <ilinyx2@gmail.com>
2023-10-07 00:44:47 +1000

    первое

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
 """Игра угадай число"""
 
 import numpy as np
-number = np.random.randint(1, 101) # загадываем число
+
+number = np.random.randint(1, 101)  # загадываем число
 # количество попыток
 count = 0
 
 while True:
-    count +=1
+    count += 1
     predict_number = int(input("Угадай число от 1 до 100: "))
     if predict_number > number:
         print("Число должно быть меньше")
@@ -14,4 +15,4 @@ while True:
         print("Число должно быть больше")
     else:
         print(f"Вы удадали число! Это число = {number}, за {count} попыток")
-        break # конец игры выход из цикла
+        break  # конец игры выход из цикла
--- game_v2.py
+++ game_v2.py
@@ -23,6 +23,7 @@ def random_predict(number: int = 1) -> int:
             break  # выход из цикла если угадали
     return count
 
+
 def binar_predict(number: int = 1) -> int:
     """_summary_
 
@@ -33,29 +34,36 @@ def binar_predict(number: int = 1) -> int:
         int: Число попыток
     """
     count = 0
-
-    while True:
+    low = 1
+    high = 101
+    while low <= high:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
+        mid = (low + high) / 2
+        if number == mid:
             break  # выход из цикла если угадали
+        elif number > mid:  # если угадываемое значение больше среднего значения диапазона
+            low = mid + 1  # задаем нижнее значение диапазона по среднему значению предыдущей итерации
+        else:  # если угадываемое значение меньше среднего значения диапазона
+            high = mid - 1  # задаем верхнее значение диапазона по среднему значению предыдущей итерации
+    # Ваш код заканчивается здесь
     return count
 
-def score_game(random_predict) -> int:
+
+def score_game(predict_alg) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        predict_alg ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    # np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=1000)  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(predict_alg(number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
